f0a1c99f9b9d80efe6c583e234ee1aa0a64e4dd9
Sundararajan T <geeksundar1993@gmail.com>
2021-05-26 20:28:54 +0530

    playbook-change

--- CFT/CFT-Deletion.sh
+++ CFT/CFT-Deletion.sh
@@ -2,6 +2,9 @@
 
 echo -e "Deleting the Stack... "
 
+export AWS_ACCESS_KEY_ID=$ACCESS_KEY && \
+export AWS_SECRET_ACCESS_KEY=$SECRET_KEY && \
+
 aws cloudformation delete-stack --stack-name $1
 
 aws cloudformation wait stack-delete-complete --stack-name $1
